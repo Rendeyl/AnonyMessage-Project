265bbaee25a45d8512d109ca2ce43ba285427f46
RenDeyl <rendey73@gmail.com>
2025-08-03 22:53:09 +0800

    Inital commit

---
+++ Front-end/AnonyMessage/src/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header() {
+  
+    return(
+        
+        <header>
+            <div className="header-cont">
+                <img id="headerLogo" src="/Temp Assets/Logo_Transparent.png" alt="Logo"/>
+                <h1 id="header-text">AnonyMessage</h1>
+                <h2 id="header-text2">Anonymous Message Sender!</h2>
+            </div>
+        </header>
+    );
+
+}
+
+export default Header;
