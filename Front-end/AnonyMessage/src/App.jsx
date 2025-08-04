cec3e695dc7c22177299788412e5c504ac36fdde
RenDeyl <rendey73@gmail.com>
2025-08-04 19:13:57 +0800

    Lower part

--- Front-end/AnonyMessage/src/App.jsx
+++ Front-end/AnonyMessage/src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./Header.jsx"
+import Button from "./Button.jsx";
 
 function App() {
   return(
+    <>
     <Header/>
+    <Button/>
+    </>
   );
 }
 
---
+++ Front-end/AnonyMessage/src/Button.jsx
@@ -0,0 +1,10 @@
+
+function Button(){
+    return(
+        <div id="lower-part">
+            <h2 id="text0">Create an account to get your own link!</h2>
+            <button id="account-btn">Get Your Own Link!</button>
+        </div>
+    );
+}
+export default Button
