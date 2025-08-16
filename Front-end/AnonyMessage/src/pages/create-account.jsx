e6850b9c53d337961d2fbae3b9e2433a060753e9
RenDeyl <rendey73@gmail.com>
2025-08-16 21:51:00 +0800

    aaa

--- Front-end/AnonyMessage/src/pages/create-account.jsx
+++ Front-end/AnonyMessage/src/pages/create-account.jsx
@@ -15,19 +15,23 @@ function Create_Account(){
 
     async function handleCreateAccount() {
 
-    if(username.length <= 2){
+    if(username.length < 3){
       alert("Username too short");
       return;
-    }else if(username.length >= 16){
+    }
+    
+    if(username.length > 16){
       alert("Username too long");
       return;
     }
 
-    if(password.length <= 4){
-      alert("Username too short");
+    if(password.length < 4){
+      alert("Password too short");
       return;
-    }else if(password.length <= 18){
-      alert("Username too long");
+    }
+    
+    if(password.length < 18){
+      alert("Password too long");
       return;
     }
 
