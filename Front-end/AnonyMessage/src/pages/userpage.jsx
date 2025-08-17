d9c2a89a7cd7c678484ed810ec72fb8842e24f49
RenDeyl <rendey73@gmail.com>
2025-08-17 23:25:30 +0800

    login fix 1.1

--- Front-end/AnonyMessage/src/pages/userpage.jsx
+++ Front-end/AnonyMessage/src/pages/userpage.jsx
@@ -1,12 +1,18 @@
 import { useParams } from "react-router-dom";
-import TextCard from "../Components/card";
+import TextCard from "../Components/card"; 
+import { useNavigate } from "react-router-dom";
 
 function UserPage(){
 
+    const navigate = useNavigate();
     const {username} = useParams();
 
     let tnd = "August  6, 2025 | 10:39PM";
     let message = "I love you";
+
+    async function logout() {
+        navigate("/");
+    }
 
     return(
 
@@ -14,7 +20,7 @@ function UserPage(){
             <header id="userHeader">
                 <img id="headerLogo2" src="/icon.png" alt="Logo"/>
                 <h1 id="displayUser">{username}</h1>
-                <button id="logoutBTN">Log out</button>
+                <button id="logoutBTN" onClick={logout}>Log out</button>
             </header>
 
             <div id="containerCard">
