e88e2f6d03d2ee79045e1325522279691f6a1565
RenDeyl <rendey73@gmail.com>
2025-08-24 01:21:54 +0800

    send 1.1

--- Front-end/AnonyMessage/src/pages/SendMessage.jsx
+++ Front-end/AnonyMessage/src/pages/SendMessage.jsx
@@ -64,17 +64,5 @@ function Sender(){
         </div>
         );
     }
-
-    return(
-        <div id="container3">
-            <img id="headerLogo" src="/icon.png" alt="Logo"/>
-
-            <h2 id="SendMessageTXT">Send Your Message!</h2>
-            <h3 id="SendMessageTXT2">Sending message to {username}</h3>
-            <textarea id="MessageInput" placeholder="Type your message here..."></textarea>
-            <button id="SubmitMessageBTN">Submit</button>
-        </div>
-    );
-
 }
 export default Sender
